06ee7ddd75a0144af248ce9ba069dbb2ed58de63
ml <ml@mlsrv.myguest.virtualbox.org>
2024-01-02 22:44:49 +0500

    add data

--- scripts/data_scripts/get_data.py
+++ scripts/data_scripts/get_data.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+
 from catboost.datasets import rotten_tomatoes
+
+# Загрузка данных
 train, test = rotten_tomatoes()
-train.to_csv("train.csv", index=False)
-test.to_csv("test.csv", index=False)
+
+# Сохранение данных
+train.to_csv("../../data/raw/train.csv", index=False)
+test.to_csv("../../data/raw/test.csv", index=False)
